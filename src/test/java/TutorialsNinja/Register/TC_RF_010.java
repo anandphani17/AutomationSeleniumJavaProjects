ff83e3c714b35943cda297eb805bb5e8f7638b28
Anand Phani <anandphani17@gmail.com>
2026-05-20 10:41:05 +0530

    TC_RF_010

---
+++ src/test/java/TutorialsNinja/Register/TC_RF_010.java
@@ -0,0 +1,69 @@
+package TutorialsNinja.Register;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
+
+public class TC_RF_010 {
+
+	
+	@Test
+	public void VerifyAccountRegistrationWithInvalidEmailAddress() throws InterruptedException, IOException {
+		
+		WebDriver driver =  new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		driver.findElement(By.xpath("//a[normalize-space()='Register']")).click();
+		driver.findElement(By.xpath("//input[@id='input-firstname']")).sendKeys("Arun");
+		driver.findElement(By.xpath("//input[@id='input-lastname']")).sendKeys("Motoori");
+		driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("amotoori");
+		driver.findElement(By.xpath("//input[@id='input-telephone']")).sendKeys("09246812111");
+		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@id='input-confirm']")).sendKeys("12345");
+		driver.findElement(By.xpath("//label[normalize-space()='Yes']")).click();
+		driver.findElement(By.xpath("//input[@name='agree']")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		Thread.sleep(3000);
+		
+		File srcScreenshot1 = driver.findElement(By.xpath("//form[@class='form-horizontal']")).getScreenshotAs(OutputType.FILE);
+		// to copy the file use filehandler from selenium
+		FileHandler.copy(srcScreenshot1, new File(System.getProperty("user.dir")+"\\Screenshots\\sc1Actual.png"));
+		
+		BufferedImage actualBImg = ImageIO.read(new File(System.getProperty("user.dir")+"\\Screenshots\\sc1Actual.png"));
+		BufferedImage expectedBImg = ImageIO.read(new File(System.getProperty("user.dir")+"\\Screenshots\\sc1Expected.png"));
+		
+		ImageDiffer imgDiffer = new ImageDiffer();
+		
+		ImageDiff ImgDifference = imgDiffer.makeDiff(expectedBImg, actualBImg);
+		boolean b = ImgDifference.hasDiff();
+		System.out.println(b);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	}
+}
